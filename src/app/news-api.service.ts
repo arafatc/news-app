e9c257e146f67ca02e96fc7adc7419a19a539ea3
arafatc <45310865+arafatc@users.noreply.github.com>
2018-12-12 23:54:29 +0100

    Initial commit

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = 'beb7450b80de4e81accc826d16180371';
+
+  constructor(private http:HttpClient) { }
+  initSources(){
+     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+} 
